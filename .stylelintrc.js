98cc9fd438b4ce7d9727aa109fe39ff311262c6e
warugaki <warugaki.web.developer@gmail.com>
2022-07-19 00:46:17 +0900

    wip: lint & format settings

---
+++ .stylelintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  plugins: [],
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-recess-order',
+    'stylelint-config-prettier',
+  ],
+  rules: {
+    'keyframes-name-pattern': null,
+    'selector-class-pattern': null,
+    'selector-id-pattern': null,
+  },
+};
